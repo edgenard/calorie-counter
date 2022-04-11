539bcd5006f28669663a3e495d3a17d9a9a9a99c
Emmanuel Genard <edgenard@gmail.com>
2022-04-11 11:44:43 -0400

    Add FoodEntry::Update

--- app/services/food_entry_management/update.rb
+++ app/services/food_entry_management/update.rb
@@ -10,15 +10,45 @@ module FoodEntryManagement
     end
 
     def call
+      validated_params = yield validate_params
+      food_entry = validated_params[:food_entry]
+      food_entry.update!(validated_params[:changes])
+      Success(food_entry)
     end
 
     private
 
     attr_reader :params
     private_class_method :new
+
+    def validate_params
+      result = UpdateContract.new.call(params)
+      if result.success?
+        Success(result.to_h)
+      else
+        error_messages = result.errors(full: true).to_h.values.join(", ")
+        Failure(error_messages)
+      end
+    end
   end
 
   class UpdateContract < Dry::Validation::Contract
+    params do
+      required(:user).value(type?: User)
+      required(:food_entry).value(type?: FoodEntry)
+      required(:changes).hash do
+        optional(:calories).value(:integer, gteq?: 0)
+        optional(:meal).value(type?: Meal)
+        optional(:eaten_at).value(:date_time)
+        optional(:name).value(:string)
+      end
+    end
+
+    rule(:user) do
+      user = values[:user]
+      food_entry = values[:food_entry]
+      base.failure("user is not authorized") unless food_entry.user == user
+    end
   end
 
   private_constant :UpdateContract
--- spec/services/food_entry_management/update_spec.rb
+++ spec/services/food_entry_management/update_spec.rb
@@ -1,4 +1,76 @@
 require "rails_helper"
 
 RSpec.describe FoodEntryManagement::Update do
+  describe ".call" do
+    let(:user) { create(:user) }
+    let(:food_entry) { create(:food_entry, user: user) }
+    let(:meal) { create(:meal, name: "new meal") }
+    context "when successful" do
+      let(:params) do
+        {
+          user: user,
+          food_entry: food_entry,
+          changes: {
+            calories: 12345,
+            meal: meal,
+            eaten_at: "2022-04-09 20:37:38",
+            name: "Orange Juice"
+          }
+        }
+      end
+      it "updates the food entry record" do
+        described_class.call(params)
+        params[:changes][:eaten_at] = params[:changes][:eaten_at].to_datetime
+
+        expect(food_entry.reload).to have_attributes(params[:changes])
+      end
+
+      it "returns success result with the updated food entry" do
+        result = described_class.call(params)
+
+        expect(result.success).to eq(food_entry.reload)
+      end
+    end
+
+    context "when food entry doesn't belong to user" do
+      let(:other_user) { create(:user) }
+      let(:params) do
+        {
+          user: other_user,
+          food_entry: food_entry,
+          changes: {
+            calories: 12345,
+            meal: meal,
+            eaten_at: "2022-04-09 20:37:38",
+            name: "Orange Juice"
+          }
+        }
+      end
+      it "returns a failure result with error message" do
+        result = described_class.call(params)
+
+        expect(result.failure).to eq("user is not authorized")
+      end
+    end
+
+    context "when params are invalid" do
+      let(:params) do
+        {
+          user: nil,
+          food_entry: food_entry,
+          changes: {
+            calories: 12345,
+            meal: meal,
+            eaten_at: "2022-04-09 20:37:38",
+            name: "Orange Juice"
+          }
+        }
+      end
+      it "returns a failure result with error message" do
+        result = described_class.call(params)
+
+        expect(result.failure).to eq("user must be User")
+      end
+    end
+  end
 end
